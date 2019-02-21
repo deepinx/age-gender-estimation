5dc84fe5595cfa04d312df82dcbd2d1bb3e4fbbd
deepinx <xcliu1893@gmail.com>
2019-02-21 11:57:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+
+## The MXNet Implementation of Face Gender and Age Estimation
+
+This repository contains a lightweight model for face gender and age estimation. The model is very small and efficient about 1MB size, 10ms on single CPU core. Gender accuracy 96% on validation set and 4.1 age MAE.
+
+Two methods ([MTCNN](https://github.com/deepinx/mtcnn-face-detection) and [ESSH](https://github.com/deepinx/enhanced-ssh-mxnet)) is both provided in this repository for face detection and alignment. For easy cases, the two methods are almost the same, however, on hard cases the ESSH method has a much better detection results. You can use ``python test.py --det 0`` to choose MTCNN method, or use ``python test.py --det 1`` for ESSH.
+
+## Environment
+
+This repository has been tested under the following environment:
+
+-   Python 2.7 
+-   Ubuntu 18.04
+-   Mxnet-cu90 (==1.3.0)
+
+## Installation
+
+1.  Prepare the environment.
+
+2.  Clone the repository.
+    
+3.  Type  `make`  to build necessary cxx libs.
+
+## Testing
+
+  -  Download the ESSH model from [baiducloud](https://pan.baidu.com/s/1sghM7w1nN3j8-UHfBHo6rA) or [googledrive](https://drive.google.com/open?id=1eX_i0iZxZTMyJ4QccYd2F4x60GbZqQQJ) and place it in *`./ssh-model/`*.
+
+  -  You can use `python test.py` to test the pre-trained models.
+
+## Training
+
+  You can use `python train.py` to train your own models.
+  
+  Training datasets will come soon.
+ 
+
+## Result
+![Estimation Result]()
+
+## License
+
+MIT LICENSE
+
+## Acknowledgment
+
+The code is adapted based on an intial fork from the [insightface](https://github.com/deepinsight/insightface) repository.
+
---
+++ test.py
@@ -0,0 +1,55 @@
+import face_model
+import argparse
+import cv2
+import sys
+import numpy as np
+import datetime
+
+
+def draw_label(image, point, label, font=cv2.FONT_HERSHEY_SIMPLEX, font_scale=1, thickness=2):
+  size = cv2.getTextSize(label, font, font_scale, thickness)[0]
+  x, y = point
+  cv2.rectangle(image, (x, y - size[1]), (x + size[0], y), (255, 0, 0), cv2.FILLED)
+  cv2.putText(image, label, point, font, font_scale, (255, 255, 255), thickness)
+
+
+parser = argparse.ArgumentParser(description='face model test')
+# general
+parser.add_argument('--image-size', default='112,112', help='')
+parser.add_argument('--image', default='sample-images/t1-4.jpg', help='')
+# parser.add_argument('--image', default='sample-images/Tom_Hanks_54745.png', help='')
+parser.add_argument('--model', default='model/model,0', help='path to load model.')
+parser.add_argument('--gpu', default=0, type=int, help='gpu id')
+parser.add_argument('--det', default=1, type=int, help='mtcnn or essh option, 0 means mtcnn, 1 means essh')
+args = parser.parse_args()
+
+model = face_model.FaceModel(args)
+#img = cv2.imread('Tom_Hanks_54745.png')
+img = cv2.imread(args.image)
+img_db, bbox, points = model.get_input(img, args)
+#f1 = model.get_feature(img)
+#print(f1[0:10])
+for _ in range(5):
+  gender, age = model.get_ga(img_db)
+time_now = datetime.datetime.now()
+count = 200
+for _ in range(count):
+  gender, age = model.get_ga(img_db)
+time_now2 = datetime.datetime.now()
+diff = time_now2 - time_now
+print('time cost', diff.total_seconds()/count)
+# if gender==0:
+#   print('gender is female')
+# else:
+#   print('gender is male')
+# print('age is %d' % age)
+
+b = bbox
+cv2.rectangle(img, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (0, 255, 0), 2)
+for p in points:
+  cv2.circle(img, (int(p[0]), int(p[1])), 1, (0, 0, 255), 2)
+label = "{}, {}".format(int(age), "F" if gender == 0 else "M")
+draw_label(img, (int(b[0]), int(b[1])), label)
+cv2.imshow("detection result", img)
+cv2.waitKey(0)
+
